54ae765069ab8196a91198970398c5d05fa9627d
Oyatillo06 <oyatillo.sharobiddinov@yandex.ru>
2022-02-07 21:58:30 +0500

    Initial commit

---
+++ Quiz/natijaapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Foydalanuvchi
+
+admin.site.register(Foydalanuvchi)
---
+++ main.py
@@ -0,0 +1,2 @@
+# 1. results.html, signup.html, login.html lar uchun viewlar natijaappga, qolganlari uchun viewlar quizappga yoziladi.
+# Login view uchun post request yozishga harakat qiling
